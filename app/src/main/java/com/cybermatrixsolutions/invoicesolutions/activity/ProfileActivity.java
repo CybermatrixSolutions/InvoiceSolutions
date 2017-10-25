d0cae4f62b2ee4d5f0e30247188077bcd7de1fa5
Cybermatrixsolutions <dhiraj.infotrench@gmail.com>
2017-10-25 17:51:21 +0530

    verify driver mobile number activity added 12

--- app/src/main/java/com/cybermatrixsolutions/invoicesolutions/activity/ProfileActivity.java
+++ app/src/main/java/com/cybermatrixsolutions/invoicesolutions/activity/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.cybermatrixsolutions.invoicesolutions.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.cybermatrixsolutions.invoicesolutions.R;
 
@@ -17,5 +19,11 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.changePassword).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),VerifyMobileNumber.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+            }
+        });
     }
 }
